8f6982c7cb390201b80b9e2af75ee750207a53fc
USER <shoutjoy1@gmail.com>
2024-03-07 21:29:05 +0900

    Round

---
+++ R/Round.R
@@ -0,0 +1,32 @@
+#' Find and round numeric variables only
+#'
+#' @param data data.frame
+#' @param digits default 2
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # vector
+#' Round(12.345654)
+#'
+#' ##data.frame
+#' mtcars%>% Round(1)
+#'
+#' }
+Round <- function(data, digits=2){
+  if(is.data.frame(data)){
+    # data = data%>%rownames_to_column( "rownames")
+
+    data = data %>% dplyr::mutate_if(is.numeric, round, digits)
+
+    #  data = data%>% column_to_rownames("rownames")
+
+  }else{
+    data <- sapply(data,
+                   function(x) {if(is.numeric(x)){round(x, digits)}})
+  }
+  data
+}
