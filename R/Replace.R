d73143283b2e4a8de2bcfc58a418e46be715cb5f
USER <shoutjoy1@gmail.com>
2024-03-27 02:56:34 +0900

    Replace

---
+++ R/Replace.R
@@ -0,0 +1,20 @@
+
+#' Create a function to replace NA with an empty string in an R vector data
+#'
+#' @param vector input data
+#' @param pattern dectect pattern
+#' @param imp imputation
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#' # Replace(c("A", NA, "B", "", "C", "C"), "C", imp="T")
+#' }
+#'
+Replace <- function(vector, pattern=NA, imp="") {
+  # Use the replace function to replace NA with an empty string
+  return(replace(vector, vector == pattern, imp))
+}
