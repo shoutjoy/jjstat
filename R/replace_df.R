63f92dee3ff77318b0506914d77aa5aef1ec07b3
USER <shoutjoy1@gmail.com>
2024-04-18 12:06:39 +0900

    replace_df2

--- R/replace_df.R
+++ R/replace_df.R
@@ -17,3 +17,29 @@ replace_df = function(df, pattern = NA, imp = "" ){
 
 
 
+#' Search for and fill in empty data
+#'
+#' @param df data.frame
+#' @param pattern default ""
+#' @param imp iimputation , defalut NA
+#'
+#' @return data.frame
+#' @export
+#'
+replace_df2 = function(df, pattern = NA, imp = "", cat=TRUE) {
+  na_counts <- colSums(is.na(df))
+  cols_with_na <- names(na_counts[na_counts > 0])
+
+  if(cat){
+    cat("\n\n",paste0("Columns with [" ,pattern, "] values:"), cols_with_na, "\n\n")
+  }
+
+
+  for (col in cols_with_na) {
+    df[[col]][is.na(df[[col]])] <- imp
+  }
+
+  return(df)
+}
+
+
