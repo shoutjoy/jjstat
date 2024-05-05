74ae4c5461ab45cff0fbee91e3d9768252cb2721
USER <shoutjoy1@gmail.com>
2024-05-05 19:42:18 +0900

    diagram2

--- R/sem_diagram.R
+++ R/sem_diagram.R
@@ -61,6 +61,8 @@ diagram = function(lav_obj, what="est",
 #' @param sizeLat  sizeLat = 10,
 #' @param style  style="lisrel"
 #' @param sig sig=TRUE
+#' @param residuals residuals=TRUE
+#' @param curve curve=1
 #' @param groups true color
 #'
 #' @return plot
@@ -72,15 +74,28 @@ diagram = function(lav_obj, what="est",
 #'
 #' example(sem)
 #' fit%>% diagram2()
+#' #'
+#' fit %>%diagram2(mar=c(3,8,3,6), sizeLat = 6, sizeMan = 6,
+#'                 edge.label.cex = 0.7)
+#'
+#' #model
+#' fit %>%diagram2("model", sig=F)
+#'
+#' fit %>%diagram2("model", sig=F, residuals=F, curve=2)
+#'
+#' fit %>%diagram2("model", sig=F, residuals=F, curve=2, rotation = 4)
+#' #'
+#'
 #'
 #' }
 #'
 #'
 diagram2 = function(lav_obj, whatLabels = "est",
-                    rotation = 4, nDigits=2,  residScale=18,
+                    rotation = 2, nDigits=2,  residScale=18,
                     mar=c(2,8,3,10),
                     edge.label.cex= 1,    sizeMan = 8, sizeMan2 = 4,
                     sizeLat = 10, style="lisrel",sig=TRUE,
+                    residuals=FALSE,curve=1,
                     groups=FALSE
 ){
 
@@ -91,7 +106,8 @@ diagram2 = function(lav_obj, whatLabels = "est",
       edge.color="black",
       rotation = rotation,
       edge.label.cex =edge.label.cex,
-      residuals= T, residScale=  residScale,
+      residuals= residuals,
+      residScale=  residScale,
       exoVar=TRUE, exoCov=TRUE,
       sizeMan = sizeMan, sizeMan2 = sizeMan2,
       sizeLat = sizeLat,
@@ -99,6 +115,7 @@ diagram2 = function(lav_obj, whatLabels = "est",
       border.width = 2,
       groups="lat",pastel=TRUE,
       edge.width=1.5,
+      curve=curve,
       nDigits = nDigits,
       style =  style,  mar=mar)
   }else{
@@ -108,13 +125,15 @@ diagram2 = function(lav_obj, whatLabels = "est",
       edge.color="black",
       rotation = rotation,
       edge.label.cex =edge.label.cex,
-      residuals= T, residScale=  residScale,
+      residuals= residuals,
+      residScale=  residScale,
       exoVar=TRUE, exoCov=TRUE,
       sizeMan = sizeMan, sizeMan2 = sizeMan2,
       sizeLat = sizeLat,
       shapeLat="circle",
       border.width = 2,
       edge.width=1.5,
+      curve=curve,
       nDigits = nDigits,
       style =  style,  mar=mar)
   }
