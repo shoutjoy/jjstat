9c4ffd8d326c02b7f82b2fb0fcac096f172f3f6c
USER <shoutjoy1@gmail.com>
2024-05-24 06:23:43 +0900

    fa_print

--- R/fa_print.R
+++ R/fa_print.R
@@ -3,6 +3,9 @@
 #' @param data suammry fa
 #' @param cut cut off 0.4
 #' @param sort TRUE FALSE
+#' @param desc decreasing=TRUE
+#' @param star star > cut
+#' @param justify justify is align "right"
 #'
 #' @return data.frame
 #' @export
@@ -25,23 +28,46 @@
 # # Example of running a function
 # fa_print(data, cut = 0.3, sort = TRUE)
 #' }
-#'
-fa_print <- function(data, cut = 0.4, sort = TRUE) {
+fa_print <- function(data, cut = 0.4,
+                     sort = TRUE,
+                     desc=TRUE, star=FALSE, justify="right") {
   # Separate data_head and data_tail by filtering rows that meet a condition
   data_head <- data[!data$source %in% c("eigen_vlaue", "Proportion_Var", "Cumulative_Var"), ]
   data_tail <- data[data$source %in% c("eigen_vlaue", "Proportion_Var", "Cumulative_Var"), ]
 
-  # Apply cut value for data_head to replace with ""
-  for (i in 2:ncol(data_head)) {
-    if (is.numeric(data_head[[i]])) {
-      data_head[[i]] <- ifelse(data_head[[i]] < cut, "", data_head[[i]])
-    } # remove abs()
+
+  if(star){
+    # Apply cut value for data_head to replace with ""
+    for (i in 2:ncol(data_head)) {
+      if (is.numeric(data_head[[i]])) {
+        data_head[[i]] <- ifelse(data_head[[i]] < cut,
+                                 format(paste0(
+                                   format(data_head[[i]],nsmall=3)," "),
+                                   nsmall = 3, width = 8,
+                                   justify=justify ),
+
+                                 format(paste0(
+                                   format(data_head[[i]],nsmall=3),"*"),
+                                   nsmall = 3, width = 8,
+                                   justify=justify ))
+      } # remove abs()
+    }
+  }else{
+    # Apply cut value for data_head to replace with ""
+    for (i in 2:ncol(data_head)) {
+      if (is.numeric(data_head[[i]])) {
+        data_head[[i]] <- ifelse(data_head[[i]] < cut, "",
+                                 format(data_head[[i]], nsmall=3))
+      } # remove abs()
+    }
   }
 
   # Sort by all columns if sort is TRUE
   if (sort) {
     # Sort only parts of data except column names
-    data_head <- data_head[do.call(order, as.list(data_head[ , -1])), ]
+    # data_head <- data_head[do.call(order, as.list(data_head[ , -1])), ]
+    data_head <- data_head[do.call(order, c(as.list(data_head[ , -1]),
+                                            list(decreasing = desc))), ]
   }
 
   # Combine the two parts back together
@@ -49,3 +75,28 @@ fa_print <- function(data, cut = 0.4, sort = TRUE) {
 
   return(data.frame(res))
 }
+# fa_print <- function(data, cut = 0.4, sort = TRUE, desc=TRUE) {
+#   # Separate data_head and data_tail by filtering rows that meet a condition
+#   data_head <- data[!data$source %in% c("eigen_vlaue", "Proportion_Var", "Cumulative_Var"), ]
+#   data_tail <- data[data$source %in% c("eigen_vlaue", "Proportion_Var", "Cumulative_Var"), ]
+#
+#   # Apply cut value for data_head to replace with ""
+#   for (i in 2:ncol(data_head)) {
+#     if (is.numeric(data_head[[i]])) {
+#       data_head[[i]] <- ifelse(data_head[[i]] < cut, "", data_head[[i]])
+#     } # remove abs()
+#   }
+#
+#   # Sort by all columns if sort is TRUE
+#   if (sort) {
+#     # Sort only parts of data except column names
+#     # data_head <- data_head[do.call(order, as.list(data_head[ , -1])), ]
+#     data_head <- data_head[do.call(order, c(as.list(data_head[ , -1]),
+#                                             list(decreasing = desc))), ]
+#   }
+#
+#   # Combine the two parts back together
+#   res <- rbind(data_head, data_tail)
+#
+#   return(data.frame(res))
+# }
