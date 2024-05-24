a65b8221e59f0cec017653a7982d48c248e63e0b
USER <shoutjoy1@gmail.com>
2024-05-25 01:47:56 +0900

    aovs

--- R/apa_aov.R
+++ R/apa_aov.R
@@ -1,35 +1,42 @@
+
 #' Report aov data (format APA)
 #'
 #' @param aov_post_data aov_post data
 #' @param md md = TRUE is markdown outpput
+#' @param ns ncol sig column 7
+#' @param np ncol p  column 6
+#' @param digits digits 3
 #'
 #' @return report data
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' Mtcars <- mtcars%>%as_trt("cyl")
-#' aov(mpg ~ cyl , data=Mtcars) %>% aov_post() %>% anova_apa()
-# aov(mpg ~ cyl , data=Mtcars) %>% aov_post(type="df") %>% anova_apa()
+#' aov(mpg ~ cyl , data=Mtcars)%>% aov_post() %>% anova_apa()
+# aov(mpg ~ cyl , data=Mtcars)%>%aov_post(type="df")%>% anova_apa()
 #' # This is error
-#' aov(mpg ~ cyl , data=Mtcars)%>% aov_post(type="contrast") %>% anova_apa()
+#' aov(mpg ~ cyl , data=Mtcars)%>%aov_post(type="contrast")%>% anova_apa()
 #'
-#' # aov(mpg ~ cyl , data=Mtcars) %>% aov_post(type="all")
+#' # aov(mpg ~ cyl , data=Mtcars)%>%aov_post(type="all")
 #'
-#' aov(mpg ~ cyl , data=Mtcars) %>% aov_post(type="all") %>% anova_apa()
-#' aov(mpg ~ cyl , data=Mtcars) %>% aov_post(type="res") %>% anova_apa()
+#' aov(mpg ~ cyl , data=Mtcars)%>%aov_post(type="all")%>% anova_apa()
+#' aov(mpg ~ cyl , data=Mtcars)%>%aov_post(type="res")%>% anova_apa()
 #'
 #' }
 #'
-anova_apa <- function(aov_post_data, np=7, ns=8, md=FALSE) {
+anova_apa <- function(aov_post_data, ns=7, np=6,
+                    digits=3, md=FALSE,
+                    posthoc_type="The Multi Least Significant Difference(MLSD)") {
+
   if(length(aov_post_data) == 8){
     stop("You shouldn't do it with 'type=contrast', do it with type='df'. do it! ")
   }
-
   data <- aov_post_data$contrast
 
   anova_dfs = aov_post_data$aov
+
   Fvalue= anova_dfs[1, 5]
+
   pvalue = ifelse(anova_dfs[1, 6] < 0.001 ,
                   "< .001).",
                   paste0("= ",round( anova_dfs[1, 6],4),")." ))
@@ -41,28 +48,30 @@ anova_apa <- function(aov_post_data, np=7, ns=8, md=FALSE) {
   iv  = form[2]
   dv  = form[3]
 
-
-
   inter <- c()
   for(i in 1:nrow(data)) { # 수정된 부분: 1부터 nrow(data)까지 반복
-    inter[i] <- paste0("가설",data[i, 1], "(", data[i, 2], ")",
+    inter[i] <- paste0("가설[",i,"]",data[i, 1], "(", round(data[i, 2], digits), ")",
                        "에 관한 차이", "분석 결과, 통계적으로",
                        ifelse(data[i, ns] == "ns", " 유의미한 효과는 없었다",
                               " 유의미한 효과가 나타났다"),
-                       "(est = ", round(data[i, 3], 3), ", p ",
+                       "(diff = ", round(data[i, 3], 3), ", p ",
                        ifelse(data[i, np]< 0.001,"< .001", paste0("= ", round(data[i, np], 3))),
-                       "). \n"
+                       ")."
     )
   }
 
   print(data)
   if(md){  print(data%>% jjstat::md(digits=3))}
 
-  cat("\n\n",paste0("'", iv,"'에 관한 '",dv, "'의 ANOVA 분석 결과 통계적으로 유의하였다(F(",df1,", ",df2,") = ",round(Fvalue, 2),", p ",
+  cat("\n\n",paste0("'", iv,"'에 관한 '",dv,
+                    "'의 ANOVA 분석 결과 통계적으로 유의하였다(F(",
+                    df1,", ",df2,") = ",round(Fvalue, 2),", p ",
                     pvalue,
                     #  format( round(pvalue,3), digits=3, scienctific=TRUE),
-                    " 이에 따라 ",  "The Least Significant Difference(LSD) 사후분석(POST HOC)을 실시하였다. 각 요인(factor)의 수준(level)에 따른 차이의 유의성 결과는 다음과 같다."),"\n\n", inter,"\n\n")
+                    " 이에 따라 ", posthoc_type ,"사후분석(POST HOC)을 실시하였다. 각 요인(factor)의 수준(level)에 따른 차이의 유의성 결과는 다음과 같다."),
+      "\n\n", inter,"\n\n")
 }
+
 
 
 
@@ -73,6 +82,7 @@ anova_apa <- function(aov_post_data, np=7, ns=8, md=FALSE) {
 #' @param md md = TRUE is markdown outpput
 #' @param ns ncol sig column 7
 #' @param np ncol p  column 6
+#' @param digits digits 3
 #'
 #' @return report data
 #' @export
@@ -91,7 +101,10 @@ anova_apa <- function(aov_post_data, np=7, ns=8, md=FALSE) {
 #'
 #' }
 #'
-aov_apa <- function(aov_post_data, ns=7, np=6, md=FALSE) {
+aov_apa <- function(aov_post_data, ns=7, np=6,
+                    digits=3, md=FALSE,
+                    posthoc_type="The Multi Least Significant Difference(MLSD)") {
+
   if(length(aov_post_data) == 8){
     stop("You shouldn't do it with 'type=contrast', do it with type='df'. do it! ")
   }
@@ -112,15 +125,13 @@ aov_apa <- function(aov_post_data, ns=7, np=6, md=FALSE) {
   iv  = form[2]
   dv  = form[3]
 
-
-
   inter <- c()
   for(i in 1:nrow(data)) { # 수정된 부분: 1부터 nrow(data)까지 반복
-    inter[i] <- paste0("가설",data[i, 1], "(", data[i, 2], ")",
+    inter[i] <- paste0("가설[",i,"]",data[i, 1], "(", round(data[i, 2], digits), ")",
                        "에 관한 차이", "분석 결과, 통계적으로",
                        ifelse(data[i, ns] == "ns", " 유의미한 효과는 없었다",
                               " 유의미한 효과가 나타났다"),
-                       "(est = ", round(data[i, 3], 3), ", p ",
+                       "(diff = ", round(data[i, 3], 3), ", p ",
                        ifelse(data[i, np]< 0.001,"< .001", paste0("= ", round(data[i, np], 3))),
                        ")."
     )
@@ -129,11 +140,12 @@ aov_apa <- function(aov_post_data, ns=7, np=6, md=FALSE) {
   print(data)
   if(md){  print(data%>% jjstat::md(digits=3))}
 
-  cat("\n\n",paste0("'", iv,"'에 관한 '",dv, "'의 ANOVA 분석 결과 통계적으로 유의하였다(F(",df1,", ",df2,") = ",round(Fvalue, 2),", p ",
+  cat("\n\n",paste0("'", iv,"'에 관한 '",dv,
+                    "'의 ANOVA 분석 결과 통계적으로 유의하였다(F(",
+                    df1,", ",df2,") = ",round(Fvalue, 2),", p ",
                     pvalue,
                     #  format( round(pvalue,3), digits=3, scienctific=TRUE),
-                    " 이에 따라 ",
-                    "The Least Significant Difference(LSD) 사후분석(POST HOC)을 실시하였다. 각 요인(factor)의 수준(level)에 따른 차이의 유의성 결과는 다음과 같다."),
+                    " 이에 따라 ", posthoc_type ,"사후분석(POST HOC)을 실시하였다. 각 요인(factor)의 수준(level)에 따른 차이의 유의성 결과는 다음과 같다."),
       "\n\n", inter,"\n\n")
 }
 
