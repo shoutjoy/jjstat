601b749d7852da8a3b98adca087456da22292508
USER <shoutjoy1@gmail.com>
2024-09-29 07:16:08 +0900

    up

---
+++ R/apa_Freq_table.R
@@ -0,0 +1,80 @@
+
+#' Interpreting frequency analysis results from Freq_table
+#'
+#' @param df data
+#' @param print df output
+#' @param digits 2
+#'
+#' @return apa
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#' #'
+#' Mtcars = mtcars
+#'
+#' Mtcars$am = factor(Mtcars$am, levels=c(0,1), labels= c("automatic","manual" ))
+#' Mtcars$vs  = factor(Mtcars$vs, levels=c(0,1), labels= c("V-shaped","straight" ))
+#' Mtcars$cyl  = factor(Mtcars$cyl, levels=c(4,6,8), labels= c("cyl-4","cyl-6","cyl-8"))
+#'
+#' Mtcars %>% Freq_table("am","vs", prop=T) %>% Freq_table_apa()
+#' Mtcars %>% table_freq("vs","am","cyl") %>% Freq_table_apa()
+#' Mtcars %>% table_freq("vs","am","cyl", angle=90)%>% Freq_table_apa()
+#'}
+#'
+Freq_table_apa <- function(df, print=TRUE, digits=2) {
+
+  # 컬럼명 추출
+  cols <- colnames(df)
+
+  # Freq와 prop(%)는 각각 "횟수"와 "비율"로 읽어냄
+  freq_col <- "Freq"
+  prop_col <- "prop(%)"
+
+  # 나머지 컬럼 처리 (Freq와 prop(%) 제외)
+  term_cols <- setdiff(colnames(df), c(freq_col, prop_col))
+
+  if (length(term_cols) > 1) {
+    # 여러 컬럼이 있으면 이를 하나의 term으로 합침, prop(%)는 제외
+    df$term_combined <- apply(df[, term_cols], 1, paste, collapse = "_")
+    term_col <- "term_combined"
+  } else {
+    term_col <- term_cols[1]
+  }
+
+  # 빈도와 비율을 포함한 경우
+  if(prop_col %in% colnames(df)) {
+    result <- "빈도분석 결과, 각빈도와 비율은 다음과 같았다. "
+
+    for (i in 1:nrow(df)) {
+      result <- paste0(result, df[[term_col]][i], "는 ", df[[freq_col]][i], "(",
+                       round(df[[prop_col]][i], digits), "%)",
+                       ifelse(i == nrow(df), "였다.", ", "))
+    }
+
+    # 빈도만 있는 경우
+  } else {
+    result <- "빈도분석 결과, 각빈도와 비율은 다음과 같았다. "
+
+    for (i in 1:nrow(df)) {
+      result <- paste0(result, df[[term_col]][i], "는 ", df[[freq_col]][i],
+                       ifelse(i == nrow(df), "이다.", ", "))
+    }
+  }
+
+  # df에서 term_combined 제거
+  df <- df[ , !(colnames(df) %in% "term_combined")]
+
+  # df를 print로 출력
+  if(print) {
+    print(df)
+  }
+
+  # 결과 출력
+  cat("\n")
+  cat(result, "\n")
+  cat("\n")
+
+
+}
--- R/lpa_bind_classdata.R
+++ R/lpa_bind_classdata.R
@@ -23,58 +23,145 @@
 #' lpa_bind_classdata(select(iris, -Species), type="data")%>%Round(3)%>%dall
 #' lpa_bind_classdata(select(iris, -Species), type="all")
 #' #'
-#'
+#' # "EEE": variances = "equal", covariances = "equal"
+#' # 프로파일 간 분산과 공분산이 모두 동일하다고 가정.
+#' # "EEV": variances = "equal", covariances = "varying"
+#' # 분산은 동일하지만, 공분산은 프로파일마다 다르게 추정.
+#' # "EEI": variances = "equal", covariances = "zero"
+#' # 분산은 동일하지만, 공분산은 추정하지 않음 (공변량 간 독립성 가정).
+#' # "EII": variances = "equal", covariances = "zero"
+#' # 동일한 가정으로 분산이 동일하고, 공분산은 추정되지 않음.
 #' }
 #'
 #'
 #'
-lpa_bind_classdata= function(data, n_profiles=3,model=NULL,
-                             package=c("mclust","MplusAutomation"),
-                             type="all",
-                             add_line=TRUE){
+lpa_bind_classdata <- function(data, n_profiles = 3, models = NULL,
+package = c("mclust", "MplusAutomation"),
+type = "all", add_line = TRUE, var_model = "EEE") {
   library(tidyLPA)
 
-  lpa_res <-  data%>%
+  # var_model에 따라 variances와 covariances 설정
+  variances <- "equal"
+  covariances <- "equal"
+
+  if (var_model == "EII") {
+    # Equal variances and covariances fixed to 0
+    variances <- "equal"
+    covariances <- "zero"
+  } else if (var_model == "VII") {
+    # Varying variances and covariances fixed to 0
+    variances <- "varying"
+    covariances <- "zero"
+  } else if (var_model == "EEE") {
+    # Equal variances and equal covariances
+    variances <- "equal"
+    covariances <- "equal"
+  } else if (var_model == "VEE") {
+    # Varying variances and equal covariances (not supported in mclust)
+    stop("VEE model is not supported in 'mclust', try with 'MplusAutomation'.")
+  } else if (var_model == "EEV") {
+    # Equal variances and varying covariances (not supported in mclust)
+    stop("EEV model is not supported in 'mclust', try with 'MplusAutomation'.")
+  } else if (var_model == "VVV") {
+    # Varying variances and varying covariances
+    variances <- "varying"
+    covariances <- "varying"
+  } else {
+    stop("Invalid var_model provided. Choose from 'EII', 'VII', 'EEE', 'VEE', 'EEV', 'VVV'.")
+  }
+
+  # 프로파일 추정
+  lpa_res <- data %>%
     single_imputation() %>%
-    estimate_profiles(n_profiles=n_profiles, package=package,model=model)
+    estimate_profiles(n_profiles = n_profiles, package = package,
+                      variances = variances, covariances = covariances)
 
-  if(type=="group_plot"){
-    lpa_res_group <-  data%>%
+  # 그룹 프로파일 플롯
+  if (type == "group_plot") {
+    lpa_res_group <- data %>%
       poms() %>%
-      estimate_profiles(n_profiles=1:n_profiles, package=package,model=model)
-     group_plot = plot_profiles(lpa_res_group, add_line=add_line)
-
+      estimate_profiles(n_profiles = 1:n_profiles, package = package,
+                        variances = variances, covariances = covariances)
+    group_plot <- plot_profiles(lpa_res_group, add_line = add_line)
   }
 
-  res_data =  get_data(lpa_res)
-  est = get_estimates(lpa_res)
-  fit = get_fit(lpa_res)
-  bivariate = plot_bivariate(lpa_res)
-  density = plot_density(lpa_res)
-  plot = plot_profiles(lpa_res, add_line=add_line)
+  # 결과 데이터 및 플롯 추출
+  res_data <- get_data(lpa_res)
+  est <- get_estimates(lpa_res)
+  fit <- get_fit(lpa_res)
+  bivariate <- plot_bivariate(lpa_res)
+  density <- plot_density(lpa_res)
+  plot <- plot_profiles(lpa_res, add_line = add_line)
 
-
-  if(type=="plot"|type=="group_plot"){
+  if (type == "plot" | type == "group_plot") {
     x11()
   }
 
-  if(type=="plot"|type=="group_plot"){
-    x11()
-  }
+  all <- list(fit = fit, estimate = est, data = res_data)
 
-  all = list(fit= fit, estimate = est, data = res_data)
-
-
-  switch(type, all= all, res= lpa_res,
-         data=res_data,
-         est =est%>%Round(3)%>%dall(),
-         fit =fit %>%dall(),
-         bivariate= bivariate,
+  # switch 문으로 결과 반환
+  switch(type,
+         all = all,
+         res = lpa_res,
+         data = res_data,
+         est = est %>% Round(3) %>% dall(),
+         fit = fit %>% dall(),
+         bivariate = bivariate,
          density = density,
          plot_density = density,
-         plot=plot,
+         plot = plot,
          plot_profiles = plot,
          plot_profiles_group = group_plot,
          group_plot = group_plot
   )
 }
+# lpa_bind_classdata= function(data, n_profiles=3,model=NULL,
+#                              package=c("mclust","MplusAutomation"),
+#                              type="all",
+#                              add_line=TRUE){
+#   library(tidyLPA)
+#
+#   lpa_res <-  data%>%
+#     single_imputation() %>%
+#     estimate_profiles(n_profiles=n_profiles, package=package,model=model)
+#
+#   if(type=="group_plot"){
+#     lpa_res_group <-  data%>%
+#       poms() %>%
+#       estimate_profiles(n_profiles=1:n_profiles, package=package,model=model)
+#      group_plot = plot_profiles(lpa_res_group, add_line=add_line)
+#
+#   }
+#
+#   res_data =  get_data(lpa_res)
+#   est = get_estimates(lpa_res)
+#   fit = get_fit(lpa_res)
+#   bivariate = plot_bivariate(lpa_res)
+#   density = plot_density(lpa_res)
+#   plot = plot_profiles(lpa_res, add_line=add_line)
+#
+#
+#   if(type=="plot"|type=="group_plot"){
+#     x11()
+#   }
+#
+#   if(type=="plot"|type=="group_plot"){
+#     x11()
+#   }
+#
+#   all = list(fit= fit, estimate = est, data = res_data)
+#
+#
+#   switch(type, all= all, res= lpa_res,
+#          data=res_data,
+#          est =est%>%Round(3)%>%dall(),
+#          fit =fit %>%dall(),
+#          bivariate= bivariate,
+#          density = density,
+#          plot_density = density,
+#          plot=plot,
+#          plot_profiles = plot,
+#          plot_profiles_group = group_plot,
+#          group_plot = group_plot
+#   )
+# }
